21de87ad1e663ad81a73c4c86d1563bc4fad4f97
Tobias Gurdan <tobias@gurdan.de>
2020-12-02 14:03:32 +0100

    Solve 2b

--- day2/day2.py
+++ day2/day2.py
@@ -1,22 +1,35 @@
 import re
 from functools import reduce
 
+
 fin = open( "input.txt" )
 regex = re.compile( r"(\d+)-(\d+)\s(\w):\s(\w*)" )
 
-def verifyLine( line ):
-    stripped = line.strip()
-    match = regex.fullmatch( stripped )
-    return verify(
+
+def verifyLine( line, fn ):
+    match = regex.fullmatch( line.strip() )
+    return fn(
         int( match.group( 1 ) ),
         int( match.group( 2 ) ),
         match.group( 3 ),
         match.group( 4 )
     )
 
+
 def verify( min, max, letter, string ):
     count = string.count( letter )
     return ( count >= min ) and ( count <= max )
 
-count = reduce( lambda count, line: count + verifyLine( line ), fin, 0 )
-print( count )
+
+def verifyToboggan( fst, snd, letter, string ):
+    count = ( string[ fst - 1 ] == letter ) + ( string[ snd - 1 ] == letter )
+    return ( count == 1 )
+
+
+countA = reduce( lambda count, line: count + verifyLine( line, verify ), fin, 0 )
+print( countA )
+
+fin.seek( 0, 0 )
+
+countB = reduce( lambda count, line: count + verifyLine( line, verifyToboggan ), fin, 0 )
+print( countB )
