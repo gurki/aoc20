966f6e4aba14f6b092cd1993af683b4672e5bb3a
Tobias Gurdan <tobias@gurdan.de>
2020-12-15 17:14:30 +0100

    Solve day11a

---
+++ day11/day11.py
@@ -0,0 +1,46 @@
+from itertools import product
+import numpy as np
+
+layout = np.array( list( map( list, open( "example.txt" ).read().split( "\n" ))))
+
+def isOccupied( layout, idx ):
+    if idx[ 0 ] < 0 or idx[ 0 ] >= layout.shape[ 0 ]:
+        return False
+    if idx[ 1 ] < 0 or idx[ 1 ] >= layout.shape[ 1 ]:
+        return False
+    return layout[ idx ] == "#"
+
+def occupiedNeighbours( layout, idx ):
+    count = 0
+    for offs in product( [ -1, 0, 1 ], repeat=2 ):
+        if offs == ( 0, 0 ): continue
+        count += isOccupied( layout, ( idx[ 0 ] + offs[ 0 ], idx[ 1 ] + offs[ 1 ] ))
+    return count
+
+def update( prev ):
+    curr = prev.copy()
+    for idx, x in np.ndenumerate( layout ):
+        if x == ".": continue
+        occ = occupiedNeighbours( layout, idx )
+        if x == "L" and occ == 0:
+            curr[ idx ] = "#"
+        if x == "#" and occ >= 4:
+            curr[ idx ] = "L"
+    return curr
+
+curr = []
+anyChange = True
+
+iter = 0
+while anyChange:
+# for iter in range( 10 ):
+    iter += 1
+    print( "iter", iter )
+    curr = update( layout )
+    print( "prev:\n", layout )
+    print( "curr:\n", curr )
+    anyChange = not np.array_equal( curr, layout )
+    layout = curr.copy()
+    print( "\n" )
+
+print( np.count_nonzero( layout == "#" ) )
