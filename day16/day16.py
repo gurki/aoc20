8a2f4bd6ee8dbd4c080a60640ec596467564989b
Tobias Gurdan <tobias@gurdan.de>
2020-12-16 15:26:02 +0100

    Condense code

--- day16/day16.py
+++ day16/day16.py
@@ -1,3 +1,6 @@
+from itertools import product
+from functools import reduce
+import operator
 import re
 
 sections = [ section.split( '\n' ) for section in open( "input.txt" ).read().split( "\n\n" ) ]
@@ -6,19 +9,16 @@ sections[2] = sections[2][1:]
 
 reField = re.compile( r"([\w\s]+): (\d+)-(\d+) or (\d+)-(\d+)")
 
-fields = {}
-
-for field in sections[0]:
-    match = reField.fullmatch( field )
-    fields[ match.group( 1 ) ] = [
-        ( int(match.group(2)), int(match.group(3)) ),
-        ( int(match.group(4)), int(match.group(5)) ),
-    ]
+fields = {
+    m.group( 1 ) : [(int(m.group(2)), int(m.group(3))), (int(m.group(4)), int(m.group(5)))]
+    for m in [ reField.fullmatch( field ) for field in sections[0] ]
+}
 
 myticket = [ int(val) for val in sections[1][0].split( ',' ) ]
 tickets = [ [ int(val) for val in ticket.split( ',' ) ] for ticket in sections[2] ]
 
 
+################################################################################
 #  part a
 
 def validField( ranges, val ):
@@ -27,51 +27,43 @@ def validField( ranges, val ):
 def hasValidField( val ):
     return any( [ validField( ranges, val ) for key, ranges in fields.items() ] )
 
-err = 0
-for ticket in tickets:
-    for val in ticket:
-        if hasValidField( val ): continue
-        err += val
-
+err = sum ( [ val for ticket in tickets for val in ticket if not hasValidField( val ) ] )
 print( err )
 
 
-# part b
+################################################################################
+#  part b
 
+# discard invalid tickets
 def validTicket( ticket ):
     return all( [ hasValidField( val ) for val in ticket ] )
 
-# discard invalid tickets
 validTickets = list( filter( validTicket, tickets ) )
 
 # collect which fields are invalid
-invalidOptions = {}
-for i in range( len( ticket ) ):
-    invalidOptions[ i ] = set()
+def gatherInvalidOptions( val ):
+    return set( [ fieldId for fieldId, ranges in fields.items() if not validField( ranges, val ) ] )
 
-for ticket in validTickets:
-    for i in range( len( ticket ) ):
-        val = ticket[ i ]
-        for fieldId, ranges in fields.items():
-            if not validField( ranges, val ):
-                invalidOptions[ i ].add( fieldId )
+invalidOptions = { i : set() for i in range( len( validTickets ) ) }
+for ticket, i in product( validTickets, range( len( myticket ) )):
+    invalidOptions[ i ].update( gatherInvalidOptions( ticket[ i ] ))
 
 # deduce actual fields by process of elimination
 fieldIds = set( fields.keys() )
 unavailable = set()
 assignment = {}
 
-for i in range( len( fieldIds ) ):
+def identifyField():
     for pos, options in invalidOptions.items():
         valids = fieldIds.difference( options.union( unavailable ) )
-        if len( valids ) != 1: continue
+        if not len( valids ) == 1: continue
         unavailable.update( valids )
-        assignment[ pos ] = list( valids )[ 0 ]
+        ( assignment[ pos ], ) = valids
+
+for i in range( len( fieldIds ) ):
+    # it is guaranteed that each run identifies at least one item
+    identifyField()
 
 # count departures
-prod = 1
-for pos, fieldId in assignment.items():
-    if not fieldId.startswith( "departure" ): continue
-    prod *= myticket[ pos ]
-
+prod = reduce( operator.mul, [ myticket[ pos ] for pos, fieldId in assignment.items() if fieldId.startswith( "departure" ) ])
 print( prod )
