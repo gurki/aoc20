fb9c1310feec62b55a94addf0ab59d118644552d
Tobias Gurdan <tobias@gurdan.de>
2020-12-17 19:02:49 +0100

    Test and debug 17b

--- day17/day17.py
+++ day17/day17.py
@@ -1,8 +1,32 @@
 from itertools import product
 import numpy as np
 import plotly.express as px
-import keyboard
 
+
+def printVoxels( voxels ):
+    lsts = list( zip( *voxels ) )
+    lower = list( map( min, lsts ) )
+    upper = list( map( max, lsts ) )
+
+    coordsZW = [ (z,w)
+        for z in range(lower[2], upper[2]+1)
+        for w in range(lower[3], upper[3]+1)
+    ]
+
+    for zw in coordsZW:
+        print( "z=" + str( zw[0] ) + ", w=" + str( zw[1] ) )
+
+        for x in range(upper[0],lower[0]-1,-1):
+            for y in range(lower[1], upper[1]+1):
+                v = (y,x,zw[0],zw[1])
+                if v in voxels:
+                    print('#', end='')
+                else:
+                    print('.', end='')
+            print('')
+        print('')
+
+    return
 
 def parseGrid( grid ):
     voxels = set()
@@ -10,7 +34,7 @@ def parseGrid( grid ):
     for idy, line in enumerate( grid ):
         for idx, val in enumerate( line ):
             if val == '.': continue
-            voxels.add( ( idx, rows - idy - 1, 0 ) )
+            voxels.add( ( idx, rows - idy - 1, 0, 0 ) )
     return voxels
 
 def drawVoxels( voxels ):
@@ -20,17 +44,17 @@ def drawVoxels( voxels ):
 
 def activeNeighbourCount( v, curr ):
     count = 0
-    for off in product( [-1, 0, 1 ], repeat=3 ):
+    for off in product( [-1, 0, 1 ], repeat=4 ):
         if off == ( 0, 0, 0 ): continue
-        w = ( v[0] + off[0], v[1] + off[1], v[2] + off[2] )
+        w = ( v[0] + off[0], v[1] + off[1], v[2] + off[2], v[3] + off[3] )
         if w in curr:
             count += 1
     return count
 
 def gatherInactiveNeighbours( v, curr, inactive ):
-    for off in product( [-1, 0, 1 ], repeat=3 ):
+    for off in product( [-1, 0, 1 ], repeat=4 ):
         if off == ( 0, 0, 0 ): continue
-        w = ( v[0] + off[0], v[1] + off[1], v[2] + off[2] )
+        w = ( v[0] + off[0], v[1] + off[1], v[2] + off[2], v[3] + off[3] )
         if w in curr: continue
         inactive.add( w )
 
@@ -58,13 +82,17 @@ def updateCycle( voxels ):
 
 
 def partA():
-    grid = open( "input.txt" ).read().split("\n")
+    grid = open( "example.txt" ).read().split("\n")
     voxels = parseGrid( grid )
-    print( voxels )
+    printVoxels( voxels )
+    # print( voxels )
 
-    for i in range(6):
+    for i in range(1):
         voxels = updateCycle( voxels )
-        drawVoxels( voxels )
         print( i, len( voxels ) )
+        # drawVoxels( voxels )
+        printVoxels( voxels )
+
+
 
 partA()
